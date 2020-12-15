d926bba812712e2c53663d81faa054bb4fbd4c37
Piero Vasquez <piero.vasquez.r@gmail.com>
2020-12-15 18:02:47 +0100

    new intro page added using ionic cli

---
+++ src/app/intro/intro.page.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
+
+interface Slide {
+  title: string;
+  subTitle: string;
+  description: string;
+  icon: string;
+  imageSrc: string;
+  imageAlt: string;
+}
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.page.html',
+  styleUrls: ['./intro.page.scss'],
+})
+export class IntroPage implements OnInit {
+  slideOps = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    centeredSlides: true,
+    speed: 400
+  };
+
+  slides: Slide[] = [
+    {
+      title: 'Escucha tu música',
+      subTitle: 'EN CUALQUIER LUGAR',
+      description: 'Los mejores álbumes, las mejores canciones. Escucha y comparte cualquier momento, a todas horas.',
+      icon: 'play',
+      imageSrc: 'assets/img/logo.png',
+      imageAlt: 'Piero Music Logo'
+    },
+    {
+      title: 'Disfruta de nuestro reproductor',
+      subTitle: 'DE VIDEOS INCREIBLES',
+      description: 'Los mejores álbumes, las mejores canciones. Escucha y comparte cualquier momento, a todas horas.',
+      icon: 'videocam',
+      imageSrc: 'assets/img/logo.png',
+      imageAlt: 'Piero Music Logo'
+    },
+    {
+      title: 'Accede al exclusivo',
+      subTitle: 'MODO DEPORTE',
+      description: 'Los mejores álbumes, las mejores canciones. Escucha y comparte cualquier momento, a todas horas.',
+      icon: 'play',
+      imageSrc: 'assets/img/logo.png',
+      imageAlt: 'Piero Music Logo'
+    }
+  ];
+  constructor(
+    private router: Router,
+    private storage: Storage
+  ) { }
+
+  ngOnInit() {
+  }
+
+  finish() {
+    this.storage.set('isIntroShowed', true);
+    this.router.navigateByUrl('/home');
+  }
+}
