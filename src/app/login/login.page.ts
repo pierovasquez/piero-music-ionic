532e4b24d2c5bbb6acbf9e39a4b87a791284aed6
Piero Vasquez <piero.vasquez.r@gmail.com>
2020-12-15 23:23:41 +0100

    login page created

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.buildForm();
+  }
+
+  ngOnInit() {
+  }
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+
+  loginUser() {
+    
+  }
+}
