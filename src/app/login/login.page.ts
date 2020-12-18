9b1936ccb071339742edab0da1ffe04affe53e6a
Piero Vasquez <piero.vasquez.r@gmail.com>
2020-12-18 21:28:43 +0100

    email and password added to the login page. Validations added as well

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -9,7 +9,16 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginPage implements OnInit {
 
   form: FormGroup;
-
+  validationMessages = {
+    email: [
+      { type: 'required', message: 'Email required' },
+      { type: 'email', message: 'This is a not valid email' }
+    ],
+    password: [
+      { type: 'required', message: 'Password required' },
+      { type: 'minlength', message: 'Password must have at least 4 characters' }
+    ]
+  };
   constructor(private formBuilder: FormBuilder) {
     this.buildForm();
   }
@@ -19,12 +28,21 @@ export class LoginPage implements OnInit {
 
   private buildForm() {
     this.form = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]]
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(4)]]
     });
   }
 
 
   loginUser() {
-    
+
+  }
+
+  get emailField() {
+    return this.form.get('email');
+  }
+
+  get passwordField() {
+    return this.form.get('password');
   }
 }
