421c4dfe5b26d167e520552d51513683c0497c3a
Piero Vasquez <piero.vasquez.r@gmail.com>
2020-12-19 16:58:58 +0100

    register function added to the authenticate service

--- src/app/services/authenticate.service.ts
+++ src/app/services/authenticate.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticateService {
 
-  constructor() { }
+  constructor(
+    private storage: Storage
+  ) { }
 
   loginUser(credentials): Promise<string> {
-    return new Promise((accept, reject) => {
+    return new Promise(async (accept, reject) => {
+      const registeredUser = await this.storage.get('user');
       if (credentials.email === 'test@test.com' && credentials.password === '12345') {
         accept('Login correcto');
+      } else if (credentials.email === registeredUser.email && credentials.password === atob(registeredUser.password)) {
+        accept('Login correct');
       } else {
         reject('Login incorrecto');
       }
     });
+  }
+
+  registerUser(userToRegister) {
+    userToRegister.password = btoa(userToRegister.password);
+    return this.storage.set('user', userToRegister);
   }
 }
