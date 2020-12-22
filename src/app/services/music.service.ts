983d2a248a820a53320a55fd2760ec9e9d57832c
Piero Vasquez <piero.vasquez.r@gmail.com>
2020-12-22 22:17:14 +0100

    artist file added and a function added to the music service to get the data from the file

--- src/app/services/music.service.ts
+++ src/app/services/music.service.ts
@@ -1,13 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import * as dataArtists from '../../assets/artists.json';
 @Injectable()
 export class MusicService {
 
   constructor(
     private http: HttpClient
   ) { }
+
+  getArtists() {
+    return dataArtists.items;
+  }
 
   getNewReleases(): Observable<any> {
     return this.http.get('https://platzi-music-api.herokuapp.com/browse/new-releases')
