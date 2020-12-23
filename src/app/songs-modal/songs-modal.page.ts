8f32270cbef5d526233657e6cf7777fd6c6ca733
Piero Vasquez <piero.vasquez.r@gmail.com>
2020-12-23 22:17:57 +0100

    songs-modal page added

---
+++ src/app/songs-modal/songs-modal.page.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { ModalController, NavParams } from '@ionic/angular';
+
+@Component({
+  selector: 'app-songs-modal',
+  templateUrl: './songs-modal.page.html',
+  styleUrls: ['./songs-modal.page.scss'],
+})
+export class SongsModalPage {
+
+  songs: any[];
+  artist: string;
+  constructor(
+    private navParams: NavParams,
+    private modalControler: ModalController
+  ) { }
+
+  // This function calls when the html of the page loaded
+  ionViewDidEnter() {
+    this.songs = this.navParams.data.songs;
+    this.artist = this.navParams.data.artist;
+  }
+
+  async selectSong(song) {
+    // This send the song to the component who called the SongsModal
+    await this.modalControler.dismiss(song);
+  }
+
+}
